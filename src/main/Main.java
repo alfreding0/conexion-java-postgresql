15f22415024fc9dde6380ca09fb14d027a5f0771
alfreding0 <alfredogonzales953@gmail.com>
2021-02-21 00:10:27 -0400

    Ejemplo de insertar, actualizar, eliliminar y mostrar registros en/de bases de datos.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,23 @@
 package main;
 
-
+import java.sql.ResultSet;
 import sql.Conexion;
 
 /** @author alfreding0 */
 public class Main {
     public static void main(String[] args) {
         Conexion con = new Conexion();
+        
+        String comando = "INSERT INTO usuario (nombre, clave) VALUES ('alfreding0', 'XD')";
+        con.ejecutarComando(comando);
+//        String comando = "UPDATE usuario SET nombre = 'alfreding0-01' WHERE id = 1; ";
+//        con.ejecutarComando(comando);
+//        String comando = "DELETE FROM usuario WHERE id = 2; ";
+//        con.ejecutarComando(comando);
+
+        String consulta = "SELECT * FROM usuario ORDER BY id DESC";
+        ResultSet rs =  con.ejecutarConsulta(consulta);
+        con.mostrarDatosPorConsola(rs);
     }
 
 }
